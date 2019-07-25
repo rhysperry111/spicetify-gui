9c9d5420930cd37d28c3e717f769252e06781813
Rhys Perry <rhysperry111@gmail.com>
2019-07-25 15:55:54 +0100

    Ok... a little bit better, but still a long way to go

--- page/js/master.js
+++ page/js/master.js
@@ -1,7 +1,35 @@
+// I am really really sorry for the long and descriptive variable names
+var iniFile `
+[Base]
+; Shadow Style
+main_fg                               = ${mainFG}
+secondary_fg                          = ${secondaryFG}
+main_bg                               = ${mainBG}
+sidebar_and_player_bg                 = ${sidebarPlayerBG}
+cover_overlay_and_shadow              = ${coverOverlayShadow}
+indicator_fg_and_button_bg            = ${indicatorFGButtonBG}
+pressing_fg                           = ${pressingFG}
+slider_bg                             = ${sliderBG}
+sidebar_indicator_and_hover_button_bg = ${sidebarIndicatorHoverButtonBG}
+scrollbar_fg_and_selected_row_bg      = ${scrollbarFGSelectedRowBG}
+pressing_button_fg                    = ${pressingButtonFG}
+pressing_button_bg                    = ${pressingButtonBG}
+selected_button                       = ${selectedButton}
+miscellaneous_bg                      = ${miscBG}
+Miscellaneous_hover_bg                = ${miscHoverBG}
+preserve_1                            = ${preserve1}
+`
+
 function downloadINI() {
   var filename = "color.ini";
-  var text = `stylesheet
-  stylesheetts
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(iniFile));
+  element.setAttribute('download', filename);
 
-  stest`
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
 }
