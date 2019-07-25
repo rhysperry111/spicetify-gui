e45cdafc437c75695fc584b833fa67e3241a6219
Rhys Perry <rhysperry111@gmail.com>
2019-07-25 16:19:14 +0100

    OMG why such long var names

--- page/js/master.js
+++ page/js/master.js
@@ -1,5 +1,15 @@
 // I am really really sorry for the long and descriptive variable names
-var iniFile `
+var mainFG, secondaryFG,
+  mainBG, sidebarPlayerBG,
+  coverOverlayShadow, indicatorFGButtonBG,
+  pressingFG, sliderBG,
+  sidebarIndicatorHoverButtonBG, scrollbarFGSelectedRowBG,
+  pressingButtonFG, pressingButtonBG,
+  selectedButton, miscBG,
+  miscHoverBG, preserve1;
+
+// I am just using a color.ini from a theme I like
+var iniFil `
 [Base]
 ; Shadow Style
 main_fg                               = ${mainFG}
@@ -20,7 +30,28 @@ Miscellaneous_hover_bg                = ${miscHoverBG}
 preserve_1                            = ${preserve1}
 `
 
+function updateVals() {
+  mainFG = document.getElementById("mainFG").value.substring(1);
+  secondaryFG = document.getElementById("secondaryFG").value.substring(1);
+  mainBG = document.getElementById("mainBG").value.substring(1);
+  sidebarPlayerBG = document.getElementById("sidebarPlayerBG").value.substring(1);
+  coverOverlayShadow = document.getElementById("coverOverlayShadow").value.substring(1);
+  indicatorFGButtonBG = document.getElementById("indicatorFGButtonBG").value.substring(1);
+  pressingFG = document.getElementById("pressingFG").value.substring(1);
+  sliderBG = document.getElementById("sliderBG").value.substring(1);
+  sidebarIndicatorHoverButtonBG = document.getElementById("sidebarIndicatorHoverButtonBG").value.substring(1);
+  scrollbarFGSelectedRowBG = document.getElementById("scrollbarFGSelectedRowBG").value.substring(1);
+  pressingButtonFG = document.getElementById("pressingButtonFG").value.substring(1);
+  pressingButtonBG = document.getElementById("pressingButtonBG").value.substring(1);
+  selectedButton = document.getElementById("selectedButton").value.substring(1);
+  miscBG = document.getElementById("miscBG").value.substring(1);
+  miscHoverBG = document.getElementById("miscHoverBG").value.substring(1);
+  preserve1 = document.getElementById("preserve1").value.substring(1);
+}
+updateVals();
+
 function downloadINI() {
+  updateVals();
   var filename = "color.ini";
   var element = document.createElement('a');
   element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(iniFile));
