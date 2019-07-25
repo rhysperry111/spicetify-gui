9b484ea642c716a3c900db0e1b6907190c1f328d
Rhys Perry <rhysperry111@gmail.com>
2019-07-25 14:05:07 +0100

    Woohoo! I'm getting *good* at this

--- page/js/master.js
+++ page/js/master.js
@@ -4,14 +4,4 @@ function downloadINI() {
   stylesheetts
 
   stest`
-  var element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  element.setAttribute('download', filename);
-
-  element.style.display = 'none';
-  document.body.appendChild(element);
-
-  element.click();
-
-  document.body.removeChild(element);
 }
